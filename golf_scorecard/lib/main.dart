480a26a134564d6255e94830d20bbfae78f34dc2
liuh2so4 <xuanyu60413@gmail.com>
2022-08-05 14:50:04 +0800

    renew the name

--- golf_scorecard/lib/main.dart
+++ golf_scorecard/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'golf',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -19,4 +19,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
