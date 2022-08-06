6c9a82410c23b473539bebbdcd767230ca5bc9a3
liuh2so4 <xuanyu60413@gmail.com>
2022-08-06 13:59:16 +0800

    scorecard UI done, function left

--- golf_scorecard/lib/main.dart
+++ golf_scorecard/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:golf_scorecard/home/home.dart';
+import 'package:golf_scorecard/history/history.dart';
+import 'package:golf_scorecard/setting/setting.dart';
+import 'package:golf_scorecard/scorecard/scorecard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +18,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const HomePage(title: 'home'),
-      //home: const History(title: 'history'),
-      //home: const Setting(title: 'setting'),
+      //home: const HomePage(title: 'home'),
+      //home: const HistoryPage(title: 'history'),
+      //home: const SettingPage(title: 'setting'),
+      home: const ScoreCardPage(
+          title: 'scorecard', course: '南寶球場', holes: 18, date: '2015/3/24'),
     );
   }
 }
--- golf_scorecard/lib/scorecard/scorecard.dart
+++ golf_scorecard/lib/scorecard/scorecard.dart
@@ -1,20 +1,103 @@
 import 'package:flutter/material.dart';
 
 class ScoreCardPage extends StatelessWidget {
-  const ScoreCardPage({Key? key, required this.title}) : super(key: key);
+  const ScoreCardPage(
+      {Key? key,
+      required this.title,
+      required this.course,
+      required this.holes,
+      required this.date})
+      : super(key: key);
+
   final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {},
-          child: const Text('History'),
+  final String course;
+  final int holes;
+  final String date;
+
+  List<Widget> _buildCells(int count, Color color, double width) {
+    return List.generate(
+      count,
+      (index) => Container(
+        alignment: Alignment.center,
+        width: width,
+        height: 60.0,
+        margin: EdgeInsets.all(4.0),
+        color: color,
+        child: Text(
+          "${index + 1}",
+          style: TextStyle(color: Colors.white, fontSize: 25),
         ),
       ),
     );
+  }
+
+  List<Widget> _buildRows(int count) {
+    return List.generate(
+      count,
+      (index) => Row(
+        children: _buildCells(holes + 1, Color.fromARGB(199, 85, 218, 77), 60),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(children: <Widget>[
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            course + ' $holes道' + ' $date',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 0, 0, 0)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:
+                      _buildCells(5, Color.fromARGB(255, 63, 130, 52), 120),
+                ),
+                Flexible(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: _buildRows(5),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 200, right: 50),
+            width: 200,
+            alignment: Alignment.center,
+            height: 60,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color.fromARGB(255, 197, 232, 0)),
+            child: Text(
+              '結算成績',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 254, 254, 254), fontSize: 30),
+            ),
+          )
+        ]));
   }
 }
