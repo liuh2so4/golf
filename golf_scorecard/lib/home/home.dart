b5a46581262faef73b543b124843cf8fca8fc317
liuh2so4 <xuanyu60413@gmail.com>
2022-08-09 22:09:12 +0800

    textfield done, input and func left

--- golf_scorecard/lib/home/home.dart
+++ golf_scorecard/lib/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:golf_scorecard/history/history.dart';
 import 'package:golf_scorecard/scorecard/scorecard.dart';
 import 'package:golf_scorecard/setting/setting.dart';
@@ -12,183 +13,247 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
-  OverlayEntry? entry;
+  bool _visible = false;
+  String _course = '';
+  int _holes = 0;
+  String _date = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        color: Color.fromARGB(197, 230, 252, 216),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 30,
-            ),
-            InkWell(
-              child: Container(
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 200,
-                      width: 300,
-                      decoration: const BoxDecoration(
-                          color: Color.fromARGB(255, 18, 71, 26),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10),
-                              bottomLeft: Radius.circular(10),
-                              bottomRight: Radius.circular(10))),
-                    ),
-                    Text(
-                      'NEW SCORECARD',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                height: 250,
-                width: 350,
-                decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 101, 212, 118),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10))),
-              ),
-              onTap: showOverlay,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              InkWell(
-                child: Container(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        height: 175,
-                        width: 125,
-                        decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 18, 71, 26),
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                topRight: Radius.circular(10),
-                                bottomLeft: Radius.circular(10),
-                                bottomRight: Radius.circular(10))),
-                      ),
-                      Text(
-                        'HISTORY',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  height: 210,
-                  width: 160,
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(97, 34, 186, 55),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10))),
-                ),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HistoryPage(
-                          title: 'history',
-                        ),
-                      ));
-                },
-              ),
-              SizedBox(
-                width: 30,
-              ),
-              InkWell(
-                child: Container(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        height: 175,
-                        width: 125,
-                        decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 18, 71, 26),
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                topRight: Radius.circular(10),
-                                bottomLeft: Radius.circular(10),
-                                bottomRight: Radius.circular(10))),
-                      ),
-                      Text(
-                        'SETTING',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  height: 210,
-                  width: 160,
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(99, 55, 205, 78),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10))),
-                ),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SettingPage(
-                                title: 'setting',
-                              )));
-                },
-              ),
-            ]),
-            SizedBox(
-              height: 30,
-            )
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
-  }
-
-  void showOverlay() {
-    entry = OverlayEntry(
-        builder: (context) => Positioned(
-            top: 280,
-            right: 100,
-            left: 100,
-            child: Container(
-                padding: EdgeInsets.only(top: 8, bottom: 8),
-                height: 200,
-                width: 200,
+        body: SingleChildScrollView(
+            child: Stack(alignment: Alignment.center, children: <Widget>[
+          InkWell(
+              splashColor: Colors.white,
+              focusColor: Colors.white,
+              highlightColor: Colors.white,
+              hoverColor: Colors.white,
+              canRequestFocus: false,
+              enableFeedback: false,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
+                  ),
+                  InkWell(
+                      child: Container(
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              height: 200,
+                              width: 300,
+                              decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 18, 71, 26),
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(10),
+                                      topRight: Radius.circular(10),
+                                      bottomLeft: Radius.circular(10),
+                                      bottomRight: Radius.circular(10))),
+                            ),
+                            Text(
+                              'NEW SCORECARD',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        height: 250,
+                        width: 350,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 101, 212, 118),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10))),
+                      ),
+                      onTap: () {
+                        if (_visible == true) {
+                          setState(() {
+                            _visible = false;
+                          });
+                        } else {
+                          setState(() {
+                            _visible = true;
+                          });
+                        }
+                      }),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        InkWell(
+                          child: Container(
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Container(
+                                  height: 175,
+                                  width: 125,
+                                  decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 18, 71, 26),
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(10),
+                                          topRight: Radius.circular(10),
+                                          bottomLeft: Radius.circular(10),
+                                          bottomRight: Radius.circular(10))),
+                                ),
+                                Text(
+                                  'HISTORY',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            height: 210,
+                            width: 160,
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(97, 34, 186, 55),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10))),
+                          ),
+                          onTap: () {
+                            if (_visible == true) {
+                              setState(() {
+                                _visible = false;
+                              });
+                            } else {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HistoryPage(
+                                      title: 'history',
+                                    ),
+                                  ));
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        InkWell(
+                          child: Container(
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Container(
+                                  height: 175,
+                                  width: 125,
+                                  decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 18, 71, 26),
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(10),
+                                          topRight: Radius.circular(10),
+                                          bottomLeft: Radius.circular(10),
+                                          bottomRight: Radius.circular(10))),
+                                ),
+                                Text(
+                                  'SETTING',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            height: 210,
+                            width: 160,
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(99, 55, 205, 78),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10))),
+                          ),
+                          onTap: () {
+                            if (_visible == true) {
+                              setState(() {
+                                _visible = false;
+                              });
+                            } else {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SettingPage(
+                                            title: 'setting',
+                                          )));
+                            }
+                          },
+                        ),
+                      ]),
+                ],
+              ),
+              onTap: () {
+                if (_visible == true) {
+                  setState(() {
+                    _visible = false;
+                  });
+                }
+              }),
+          Visibility(
+              visible: _visible,
+              child: Container(
+                padding: EdgeInsets.all(18),
                 decoration: BoxDecoration(
-                    color: Color.fromARGB(117, 22, 137, 41),
-                    borderRadius: BorderRadius.circular(20)),
+                    borderRadius: BorderRadius.circular(12),
+                    color: Color.fromARGB(164, 100, 184, 100)),
+                height: 250,
+                width: 200,
                 child: Column(children: [
-                  //對話框*3
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(208, 178, 238, 181),
+                        hintText: '球場',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(208, 178, 238, 181),
+                        hintText: '道數',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(208, 178, 238, 181),
+                        hintText: '日期',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
                   ElevatedButton.icon(
                     icon: Icon(Icons.stop_circle_rounded),
                     onPressed: () {
@@ -201,8 +266,9 @@ class _HomePage extends State<HomePage> {
                                 holes: 18,
                                 date: '2015/3/24'),
                           ));
-                      entry?.remove();
-                      entry = null;
+                      setState(() {
+                        _visible = false;
+                      });
                     },
                     label: Text(
                       '確定',
@@ -212,9 +278,8 @@ class _HomePage extends State<HomePage> {
                           fontSize: 20),
                     ),
                   ),
-                ]))));
-
-    final overlay = Overlay.of(this.context)!;
-    overlay.insert(entry!);
+                ]),
+              ))
+        ])));
   }
 }
